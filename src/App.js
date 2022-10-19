a0e4d61c1eaaea03d3f270e9b1d40c17602cfaa7
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-10-19 14:15:44 +0200

    Define global styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab autem impedit laborum maiores nulla reiciendis repudiandae sed unde ut! Aliquam asperiores cupiditate dolore dolorem iure laboriosam modi perferendis reiciendis, voluptatum?
     </div>
   );
 }
