6889fb42c1120133e6d45702aea9d813488ca34b
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-10-25 18:36:06 +0200

    Particles animation

---
+++ src/components/particles/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './index.scss'
+
+const Animation = () => {
+ const item=[];
+  let i;
+  for (i = 0; i < 400; i++) {
+      item.push(' ')
+  }
+  return (
+    <div className={'wrap'}>
+      {item.map(i => (
+        <div className={'c'}>{i.item}</div>
+      ))}
+    < /div>
+  )
+
+
+}
+export default Animation
