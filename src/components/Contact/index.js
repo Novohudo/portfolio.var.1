dcfd2fa3e08819de4b4a6e036b976ecd87f151ac
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-10-28 19:54:34 +0200

    React-leaflet

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,38 @@
 import './index.scss'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+    .sendForm(
+      'service_n06offg',
+      'template_87wfe5e',
+      refForm.current,
+      'WMWAlnGwL52J3lYvG',
+    ).then(() => {
+        alert('Message successfully sent!')
+      window.location.reload(false)
+      },
+      () => {
+        alert('Failed to send the message, please try again')
+      });
+  };
+
 
   return (
     <>
@@ -28,7 +50,7 @@ const Contact = () => {
             sapiente tenetur?
           </p>
           <div className={'contact-form'}>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className={'half'}>
                   <input
@@ -71,6 +93,22 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className={"info-map"}>
+          Aleksey Umrikhin,
+          <br/>
+          Poland,
+          <br/>
+          Zalasewo os.Wladyslawa Zamoyskiego 8, 62-020 <br/>
+          <span>umrixinaleksey@gmail.com</span>
+        </div>
+        <div className={'map-wrap'}>
+          <MapContainer center={[52.390280688980596,17.07446196728249]} zoom={13}>
+          <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}/>
+            <Marker position = {[52.390280688980596,17.07446196728249]}>
+              <Popup>Aleks lives here , come over for a cup coffee :) </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type={'pacman'} />
