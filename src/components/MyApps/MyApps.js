cd3c09a1dc8b3a5c47a5fddc8bfb712d0dbad656
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-11-03 19:20:15 +0100

    MyApps component

--- src/components/MyApps/MyApps.js
+++ src/components/MyApps/MyApps.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import './MyApps.scss'
+import React, { useEffect,useState } from 'react'
+import './MyApps.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowsToDot } from '@fortawesome/free-solid-svg-icons'
+import { faArrowsToDot, faListCheck, faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import Loader from 'react-loaders'
 
 const MyApps = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+
   return (
-    <div className={"myapps"}>
-      <ul className={"link-wrap"}>
-        <li className={"link-item"}>
-          <a href={"https://novohudo.github.io/posts"} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon icon={faArrowsToDot}/> </a>
+    <>
+    <div className={'container apps-page'}>
+      <div className={'text-zone'}>
+        <h1>
+          <AnimatedLetters
+            lettersClass={letterClass}
+            strArray={['M', 'y', ' ', 'p', 'r', 'a', 'c','t','i','c','e',' ','projects']}
+            idx={15}/>
+        </h1>
+      </div>
+      <ul className={'links-list'}>
+        <li>
+          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
+            icon={faUserFriends} color={'#4d4d4e'} /> </a>
         </li>
-        <li className={"link-item"}>
-          <a href={"https://novohudo.github.io/posts"} target={'_blank'} rel={'noreferrer'}> {' '}</a>
+        <li>
+          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}> <FontAwesomeIcon
+            icon={faListCheck} color={'#4d4d4e'} /></a>
         </li>
-        <li className={"link-item"}>
-          <a href={"https://novohudo.github.io/posts"} target={'_blank'} rel={'noreferrer'}> {' '}</a>
+        <li>
+          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
+            icon={faArrowsToDot} color={'#4d4d4e'} /></a>
         </li>
-        <li className={"link-item"}>
-          <a href={"https://novohudo.github.io/posts"} target={'_blank'} rel={'noreferrer'}>{' '}</a>
+        <li>
+          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
+            icon={faArrowsToDot} color={'#4d4d4e'} /></a>
         </li>
       </ul>
     </div>
+
+  <Loader type={'pacman'}/>
+  </>
   )
 }
 
