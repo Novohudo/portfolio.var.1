6fc73b3444eead5600a629b59f483ebb1e5be0c3
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-11-04 19:38:28 +0100

    @media test on mobile dev

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ import Loader from 'react-loaders'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ['l', 'e', 'k', 's', 'e', 'y']
-  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e','v','e','l','o','p','e','r', '.']
 
   useEffect(() => {
      setTimeout(() => {
