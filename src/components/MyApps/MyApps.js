510db5c7d92929b785597929217f8a9a4b9589be
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2022-11-22 15:47:35 +0100

    adaptive.fix

--- src/components/MyApps/MyApps.js
+++ src/components/MyApps/MyApps.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './MyApps.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowsToDot, faListCheck, faUserFriends } from '@fortawesome/free-solid-svg-icons'
@@ -17,31 +17,30 @@ const MyApps = () => {
 
   return (
     <>
-    <div className={'container apps-page'}>
-      <div className={'text-zone'}>
-        <h1>
-          <AnimatedLetters
-            lettersClass={letterClass}
-            strArray={['M', 'y', ' ', 'p', 'r', 'a', 'c','t','i','c','e',' ','projects']}
-            idx={15}/>
-        </h1>
-      </div>
-      <div className={'links-list'}>
-        <div className={'links-row'}>
-          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
-            icon={faUserFriends} color={'#4d4d4e'} /></a>
-          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}> <FontAwesomeIcon
-            icon={faListCheck} color={'#4d4d4e'} /></a>
-          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
-            icon={faArrowsToDot} color={'#4d4d4e'} /></a>
-          <a href={'https://novohudo.github.io/posts'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
-            icon={faArrowsToDot} color={'#4d4d4e'} /></a>
+      <div className={'container apps-page'}>
+        <div className={'text-zone'}>
+          <h1>
+            <AnimatedLetters
+              lettersClass={letterClass}
+              strArray={['M', 'y', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', ' ', 'projects']}
+              idx={15} />
+          </h1>
+        </div>
+        <div className={'links-list'}>
+          <div className={'links-row'}>
+            <a href={'https://novohudo.github.io/Social-Network/'} target={'_blank'} rel={'noreferrer'}><FontAwesomeIcon
+              icon={faUserFriends} color={'#4d4d4e'} /><p>"Social-Network"- with:
+              React-Redux<br/>React-Router<br/>Redux-Form<br/>Redux-Thunk<br/>Sass<br />My first steps and practice :)</p></a>
+            <a href={'https://novohudo.github.io/ToDo.colorfull/'} target={'_blank'} rel={'noreferrer'}>
+              <FontAwesomeIcon
+                icon={faListCheck} color={'#4d4d4e'} /><p>"ToDo-list"- work on mistakes and improved code style..</p>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-  <Loader type={'pacman'}/>
-  </>
+      <Loader type={'pacman'} />
+    </>
   )
 }
 
